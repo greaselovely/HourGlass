df76c384a72ca9ca9a743c3b1e4f47da2aebb98f
Giggly Bits <71092239+greaselovely@users.noreply.github.com>
2023-10-06 11:56:37 -0600

    Update vla.py

--- vla.py
+++ vla.py
@@ -11,9 +11,6 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 url = "https://public.nrao.edu/wp-content/uploads/temp/vla_webcam_temp.jpg"
 
-output_path = Path.joinpath(Path.home(), "VLA")
-if not output_path.exists(): output_path.mkdir(parents=True)
-
 class ImageDownloader:
     def __init__(self, out_path):
         self.out_path = Path(out_path)
@@ -21,8 +18,8 @@ class ImageDownloader:
         self.prev_image_size = None
 
     def download_image(self, img_url):
-        today_short_date = datetime.now().strftime("%m%d%Y")
-        today_short_time = datetime.now().strftime("%H%M%S")
+        TodayShortDate = datetime.now().strftime("%m%d%Y")
+        TodayShortTime = datetime.now().strftime("%H%M%S")
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"}
         r = requests.get(img_url, headers=headers, verify=False)
         
@@ -34,7 +31,7 @@ class ImageDownloader:
 
             # Compare sizes and save the new image only if it's different
             if current_image_size != self.prev_image_size:
-                FileName = f'vla.{str(today_short_date)}.{str(today_short_time)}.jpg'
+                FileName = f'vla.{str(TodayShortDate)}.{str(TodayShortTime)}.jpg'
                 with open(self.out_path / FileName, 'wb') as f:
                     f.write(r.content)
                 # Update the previous image filename and size for the next run
@@ -42,7 +39,7 @@ class ImageDownloader:
                 self.prev_image_size = current_image_size
         else:
             # If there's no previous image or it's different, save the current one
-            FileName = f'vla.{str(today_short_date)}.{str(today_short_time)}.jpg'
+            FileName = f'vla.{str(TodayShortDate)}.{str(TodayShortTime)}.jpg'
             with open(self.out_path / FileName, 'wb') as f:
                 f.write(r.content)
             # Update the previous image filename and size for the next run
@@ -55,10 +52,9 @@ def clear():
 def activity(char):
     print(char, end="\r", flush=True)
 
-def images_to_video(outputh_path, output_path, fps):
-    # images = sorted([img for img in os.listdir(outputh_path) if img.endswith(".jpg")])
-    images = sorted([img for img in Path.iterdir(outputh_path) if img.endswith(".jpg")])
-    frame = cv2.imread(Path.joinpath(outputh_path, images[0]))
+def images_to_video(image_folder, output_path, fps):
+    images = sorted([img for img in os.listdir(image_folder) if img.endswith(".jpg")])
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, _ = frame.shape
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
@@ -66,7 +62,7 @@ def images_to_video(outputh_path, output_path, fps):
 
     for image in images:
         print(image)
-        img_path = Path.joinpath(outputh_path, image)
+        img_path = os.path.join(image_folder, image)
         frame = cv2.imread(img_path)
         video.write(frame)
 
@@ -78,6 +74,7 @@ def main():
         clear()
         cursor.hide()
         i = 1
+        output_path = os.path.join(os.path.expanduser('~'), "VLA")
         downloader = ImageDownloader(output_path)
         while True:
             downloader.download_image(url)
@@ -85,10 +82,12 @@ def main():
             sleep(15)
             i += 1
     except KeyboardInterrupt:
-        today_short_date = datetime.now().strftime("%m%d%Y")
-        video_output_path = Path.joinpath(output_path, f"VLA.{today_short_date}.mp4")
+        TodayShortDate = datetime.now().strftime("%m%d%Y")
+        home = os.path.expanduser('~')
+        image_folder = f"{home}/VLA"
+        output_path = f"{home}/VLA/VLA.{TodayShortDate}.mp4"
         fps = 10
-        images_to_video(output_path, video_output_path, fps)
+        images_to_video(image_folder, output_path, fps)
         cursor.show()
 
 if __name__ == "__main__":
