2746d3ab30db694eeab6865bf7de35478d291bc6
Giggly Bits <71092239+greaselovely@users.noreply.github.com>
2023-10-17 11:13:21 -0600

    Update vla.py

--- vla.py
+++ vla.py
@@ -10,6 +10,9 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 url = "https://public.nrao.edu/wp-content/uploads/temp/vla_webcam_temp.jpg"
+webpage = "https://public.nrao.edu/vla-webcam/"
+
+proxies = {'http': 'None', 'https': 'None'}
 
 class ImageDownloader:
     def __init__(self, out_path):
@@ -17,32 +20,32 @@ class ImageDownloader:
         self.prev_image_filename = None
         self.prev_image_size = None
 
-    def download_image(self, img_url):
+    def download_image(self, filename):
+        print(proxies)
         TodayShortDate = datetime.now().strftime("%m%d%Y")
         TodayShortTime = datetime.now().strftime("%H%M%S")
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"}
-        r = requests.get(img_url, headers=headers, verify=False)
-        
-        # Check if a previous image exists and if it's the same as the current one
+        r = requests.get(filename, proxies=proxies, headers=headers, verify=False)
+
+        if len(r.content) == 0:
+            print(f"\tError: file size = 0.", end='\r')
+            return
+
         if self.prev_image_filename and (self.out_path / self.prev_image_filename).exists():
             prev_image_path = self.out_path / self.prev_image_filename
             self.prev_image_size = prev_image_path.stat().st_size
             current_image_size = len(r.content)
 
-            # Compare sizes and save the new image only if it's different
             if current_image_size != self.prev_image_size:
                 FileName = f'vla.{str(TodayShortDate)}.{str(TodayShortTime)}.jpg'
                 with open(self.out_path / FileName, 'wb') as f:
                     f.write(r.content)
-                # Update the previous image filename and size for the next run
                 self.prev_image_filename = FileName
                 self.prev_image_size = current_image_size
         else:
-            # If there's no previous image or it's different, save the current one
             FileName = f'vla.{str(TodayShortDate)}.{str(TodayShortTime)}.jpg'
             with open(self.out_path / FileName, 'wb') as f:
                 f.write(r.content)
-            # Update the previous image filename and size for the next run
             self.prev_image_filename = FileName
             self.prev_image_size = len(r.content)
 
@@ -82,13 +85,16 @@ def main():
             sleep(15)
             i += 1
     except KeyboardInterrupt:
-        TodayShortDate = datetime.now().strftime("%m%d%Y")
-        home = os.path.expanduser('~')
-        image_folder = f"{home}/VLA"
-        output_path = f"{home}/VLA/VLA.{TodayShortDate}.mp4"
-        fps = 10
-        images_to_video(image_folder, output_path, fps)
-        cursor.show()
+        try:
+            TodayShortDate = datetime.now().strftime("%m%d%Y")
+            home = os.path.expanduser('~')
+            image_folder = f"{home}/VLA"
+            output_path = f"{home}/VLA/VLA.{TodayShortDate}.mp4"
+            fps = 10
+            images_to_video(image_folder, output_path, fps)
+            cursor.show()
+        except:
+            print("[!]\t Nothing to process.  Closing.")
 
 if __name__ == "__main__":
     main()
