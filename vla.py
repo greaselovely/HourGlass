963b29c7b2b490eba0fcb9b783240d068dfb3f5e
Giggly Bits <shayne@fennon.com>
2023-11-20 10:45:16 -0700

    cleanup

--- vla.py
+++ vla.py
@@ -26,9 +26,6 @@ user_agents = [
     "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36"
 ]
 
-
-
-
 proxies = {'http': 'http://10.29.60.103:3128', 'https': 'http://10.29.60.103:3128'}
 
 class ImageDownloader:
