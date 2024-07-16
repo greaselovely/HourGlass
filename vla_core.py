13e2304990db55a5a213b63f8ea6a0ff555ad0a0
Shayne Sivley <shayne@fennon.com>
2024-07-15 20:07:01 -0600

    2.0
    Attempting to download image if it's the same hash after 5 seconds

--- vla_core.py
+++ vla_core.py
@@ -110,7 +110,7 @@ class ImageDownloader:
             if self.prev_image_hash == image_hash:
                 if attempt == 0:  # If it's the first attempt
                     logging.info(f"{RED_CIRCLE} Code: {r.status_code} Same Hash: {image_hash}. Retrying in {retry_delay} seconds...")
-                    time.sleep(retry_delay)
+                    sleep(retry_delay)
                     continue  # Try again
                 else:
                     logging.info(f"{RED_CIRCLE} Code: {r.status_code} Same Hash: {image_hash} after retry. Skipping.")
