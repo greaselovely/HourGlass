e64447aa8f37382b0f592cc730fe097ac53dda7d
Shayne Sivley <shayne@fennon.com>
2024-09-04 21:02:52 -0600

    Time And Date Config Update
    timeanddate.com updated their URL to use lat and long from the previous URL pattern they had, update it to the VLA lat and long

--- vla_config.py
+++ vla_config.py
@@ -191,7 +191,8 @@ def load_config():
                 "Mozilla/5.0 (Android 11; Mobile; rv:100.0) Firefox/100.0",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50",
                 "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36"
-            ]
+            ],
+            "ntfy": "http://ntfy.sh/"
         }
 
     def update_config(config):
